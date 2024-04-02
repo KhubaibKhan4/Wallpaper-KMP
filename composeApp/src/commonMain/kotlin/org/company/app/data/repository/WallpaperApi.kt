c6961336e0da693305259543a0d7a0ad0e1a176d
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-02 12:28:25 +0500

    -Wallpaper Api Impl (#39)

--- composeApp/src/commonMain/kotlin/org/company/app/data/repository/WallpaperApi.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/data/repository/WallpaperApi.kt
@@ -2,6 +2,6 @@ package org.company.app.data.repository
 
 import org.company.app.domain.model.Wallpaper
 
-interface Repository {
+interface WallpaperApi {
     suspend fun getWallpapers(per_page: Int, page: Int): Wallpaper
 }
--- composeApp/src/commonMain/kotlin/org/company/app/domain/repository/Repository.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/domain/repository/Repository.kt
@@ -1,10 +1,11 @@
 package org.company.app.domain.repository
 
-import org.company.app.domain.model.Wallpaper
 import org.company.app.data.remote.WallpaperClientApi
+import org.company.app.data.repository.WallpaperApi
+import org.company.app.domain.model.Wallpaper
 
-class Repository {
-    suspend fun getWallpaper(page: Int, per_page: Int): Wallpaper {
+class Repository : WallpaperApi {
+    override suspend fun getWallpapers(per_page: Int, page: Int): Wallpaper {
         return WallpaperClientApi.getWallpapers(per_page, page)
     }
 }
