789f779cdb372967d0b0b1a6c66cea3a21110d6c
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-02 12:37:49 +0500

    -View Model Impl (#45)

--- composeApp/src/commonMain/kotlin/org/company/app/presentation/viewmodel/MainViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/presentation/viewmodel/MainViewModel.kt
@@ -1,7 +1,6 @@
 package org.company.app.presentation.viewmodel
 
-import com.rickclephas.kmm.viewmodel.KMMViewModel
-import com.rickclephas.kmm.viewmodel.coroutineScope
+import dev.icerock.moko.mvvm.viewmodel.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -9,13 +8,13 @@ import org.company.app.domain.model.Wallpaper
 import org.company.app.domain.repository.Repository
 import org.company.app.domain.usecase.ResultState
 
-class MainViewModel(private val repository: Repository) : KMMViewModel() {
+class MainViewModel(private val repository: Repository) : ViewModel() {
 
     private val _wallpaperState = MutableStateFlow<ResultState<Wallpaper>>(ResultState.Loading)
     val wallpapers: StateFlow<ResultState<Wallpaper>> = _wallpaperState
 
     fun getWallpapers(page: Int, per_pag: Int) {
-        viewModelScope.coroutineScope.launch {
+        viewModelScope.launch {
             _wallpaperState.value = ResultState.Loading
             try {
                 val response = repository.getWallpapers(page, per_pag)
