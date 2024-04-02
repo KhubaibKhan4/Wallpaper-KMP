66d29276781aacd41a1553a765141b4ebfc11eda
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-02 12:47:33 +0500

    -Koin Impl on Jvm & Js (#50)

--- composeApp/src/jvmMain/kotlin/main.kt
+++ composeApp/src/jvmMain/kotlin/main.kt
@@ -3,9 +3,14 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import org.company.app.App
+import org.company.app.di.appModule
+import org.koin.core.context.startKoin
 import java.awt.Dimension
 
 fun main() = application {
+    startKoin {
+        modules(appModule)
+    }
     Window(
         title = "Wallpaper KMP",
         state = rememberWindowState(width = 800.dp, height = 600.dp),
